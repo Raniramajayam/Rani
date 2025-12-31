56d35ca3e4337c3a9d819651978279386295d2c3
Raniramajayam <ranijayam93@gmail.com>
2025-12-31 14:04:25 +0530

    sub update

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,4 +1,4 @@
-from src.calc import add,mul
+from src.calc import add,mul,sub
 
 # a = int(input("Enter first number: "))
 # b = int(input("Enter second number: "))
@@ -6,9 +6,11 @@ from src.calc import add,mul
 # sum=add(a,b)
 # multi=mul(a,b)
 def test_add():
-    assert add(1000, 2) == 1006
+    assert add(1000, 2) == 1002
 def test_mul():
-    assert mul(1000, 2) == 2004
+    assert mul(1000, 2) == 2000
+def test_sub():
+    assert sub(1000, 200) == 800
 
 
 
